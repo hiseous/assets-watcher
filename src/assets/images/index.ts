52d7a6fed2ab52700b20d09f98aa3e6903b3100d
Hiseous <hiseous@gmail.com>
2025-05-27 08:56:30 +0100

    refac image watcher

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,5 @@
 import bluetoothPng from './items/bluetooth.png';
-import forSchools2Jpg from './items/for-schools-2.jpg';
 
 export const __imageAssets = {
     bluetoothPng,
-    forSchools2Jpg,
 };
